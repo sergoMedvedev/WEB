ac03f424035178409f604906b31140d52d2543b5
sergeyMedvedev <sm@zero-ediscovery.ru>
2024-11-20 12:24:00 +0300

    update init.sql

--- lab_4/db/init.sql
+++ lab_4/db/init.sql
@@ -15,9 +15,17 @@ CREATE TABLE footballer (
 	rating 			INT					  NOT NULL
 );
 
+CREATE TABLE coach (
+	coach_id			SERIAL 				PRIMARY KEY,
+	first_name	    CHARACTER VARYING(30) 	NOT NULL,
+	last_name 		CHARACTER VARYING(30) 	NOT NULL
+);
+
 CREATE TABLE football_club (
 	football_club_id 	SERIAL PRIMARY KEY,
-	name_clab CHARACTER VARYING(30)  NOT NULL
+	name_clab CHARACTER VARYING(30)  NOT NULL,
+	id_coach INT NOT NULL,
+	CONSTRAINT fk_coach FOREIGN KEY(id_coach) REFERENCES coach(coach_id)
 );
 
 CREATE TABLE squad (
@@ -30,13 +38,6 @@ CREATE TABLE squad (
 	CONSTRAINT fk_football_club FOREIGN KEY(id_football_club) REFERENCES football_club(football_club_id)
 );
 
-CREATE TABLE coach (
-	coach_id			SERIAL 				PRIMARY KEY,
-	first_name	    CHARACTER VARYING(30) 	NOT NULL,
-	last_name 		CHARACTER VARYING(30) 	NOT NULL,
-	id_football_club	INT					NOT NULL,
-	CONSTRAINT fk_football_club FOREIGN KEY(id_football_club) REFERENCES football_club(football_club_id)
-);
 
 CREATE TABLE squad_football_club (
 	id_squad INT NOT NULL,
@@ -44,6 +45,3 @@ CREATE TABLE squad_football_club (
 	CONSTRAINT fk_squad FOREIGN KEY(id_squad) REFERENCES squad(squad_id),
 	CONSTRAINT fk_football_club FOREIGN KEY(id_football_club) REFERENCES football_club(football_club_id)
 );
-
-
-
