82a91b5d241682327d26bf938a22b4b461e89fba
sergoMedvedev <sergo.ranes1@mail.ru>
2024-11-28 11:54:59 +0300

    подключил базу и инициализоровал ее

--- lab_4/server/src/main.go
+++ lab_4/server/src/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"lab_4/server/src/settings"
+
+	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"lab_4/server/src/settings"
 )
 
 var DBinst *sqlx.DB
+var R *gin.Engine
 
 func main() {
+
+	//инициализация настроек
 	settings.InitializeFromEnv()
-	settings.InitializePostgres(DBinst)
+	settings.InitializePostgres()
+
+	R = gin.Default()
 }
--- lab_4/server/src/settings/settingsdb.go
+++ lab_4/server/src/settings/settingsdb.go
@@ -6,16 +6,68 @@ import (
 	"log"
 )
 
-func InitializePostgres(db *sqlx.DB) {
+var DBinst *sqlx.DB
+var initDB string = `
+CREATE TABLE costumer (
+	user_id 		SERIAL                PRIMARY KEY,
+	first_name	    CHARACTER VARYING(30) NOT NULL,
+	last_name 		CHARACTER VARYING(30) NOT NULL,
+	login			CHARACTER VARYING(30) NOT NULL,
+	password 		CHARACTER VARYING(30) NOT NULL,
+	access			CHARACTER VARYING(30) NOT NULL
+);
+
+CREATE TABLE footballer (
+	footballer_id 	SERIAL PRIMARY KEY,
+	first_name	    CHARACTER VARYING(30) NOT NULL,
+	last_name 		CHARACTER VARYING(30) NOT NULL,
+	position 		CHARACTER VARYING(3)  NOT NULL,
+	rating 			INT					  NOT NULL
+);
+
+CREATE TABLE coach (
+	coach_id			SERIAL 				PRIMARY KEY,
+	first_name	    CHARACTER VARYING(30) 	NOT NULL,
+	last_name 		CHARACTER VARYING(30) 	NOT NULL
+);
+
+CREATE TABLE football_club (
+	football_club_id 	SERIAL PRIMARY KEY,
+	name_clab CHARACTER VARYING(30)  NOT NULL,
+	id_coach INT NOT NULL,
+	CONSTRAINT fk_coach FOREIGN KEY(id_coach) REFERENCES coach(coach_id)
+);
+
+CREATE TABLE squad (
+	squad_id		SERIAL PRIMARY KEY,
+	name_squad	CHARACTER VARYING(30)			NOT NULL,
+	rating_squad	  INT						NOT NULL,
+	id_footballer	  INT,
+	id_football_club  INT,
+	CONSTRAINT fk_footballer FOREIGN KEY(id_footballer) REFERENCES footballer(footballer_id),
+	CONSTRAINT fk_football_club FOREIGN KEY(id_football_club) REFERENCES football_club(football_club_id)
+);
+
+
+CREATE TABLE squad_football_club (
+	id_squad INT NOT NULL,
+	id_football_club INT NOT NULL,
+	CONSTRAINT fk_squad FOREIGN KEY(id_squad) REFERENCES squad(squad_id),
+	CONSTRAINT fk_football_club FOREIGN KEY(id_football_club) REFERENCES football_club(football_club_id)
+);
+`
+
+func InitializePostgres() {
 	//configUrl := "postgresql://localhost:15432/test"
-	db, err := sqlx.Open("postgres", "user=postgres password=password dbname=test sslmode=disable")
+	db, err := sqlx.Connect("postgres", "user=postgres password=password dbname=test sslmode=disable")
 	if err != nil {
 		log.Fatalln("Error when connect db: ", err.Error())
 	}
+	DBinst = db
 	log.Println("Successfully connected to postgres")
-
+	initPostgresDataBase(db)
 }
 
 func initPostgresDataBase(db *sqlx.DB) {
-
+	db.Exec(initDB)
 }
