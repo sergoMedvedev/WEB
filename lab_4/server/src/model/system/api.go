8dec5af104a060e0fcd10f6f7c456f37d369b733
sergeyMedvedev <sm@zero-ediscovery.ru>
2024-12-02 12:47:37 +0300

    update rep

--- lab_4/server/src/model/system/api.go
+++ lab_4/server/src/model/system/api.go
@@ -1,75 +1,57 @@
 package system
 
 import (
-	"fmt"
 	"lab_4/server/src/model/footballer"
 	"lab_4/server/src/settings"
 	"log"
-	"strings"
+	"time"
 )
 
 func CreateFootballClubAndSquad(request CreateFootbalClubAndSquadRequest) (CreateFootbalClubAndSquadRequest, error) {
 	tx, err := settings.DBinst.Begin()
 	if err != nil {
-		log.Println("создание транзакции")
+		log.Println("создание транзакции", err.Error())
 		return CreateFootbalClubAndSquadRequest{}, err
 	}
 	// 1. Создание футбольного клуба
 	var clubID int
 	err = tx.QueryRow(`
 		INSERT INTO football_club (name_club, id_coach)
-		VALUES ($1, $2)`, request.ClubName, request.CoachID).Scan(&clubID)
+		VALUES ($1, $2)
+		RETURNING football_club_id`, request.ClubName, request.CoachID).Scan(&clubID)
 	if err != nil {
-		log.Println("ошибка при создании футбольного клуба")
+		log.Println("ошибка при создании футбольного клуба", err.Error())
 		return CreateFootbalClubAndSquadRequest{}, err
 	}
 
 	//найдем рейтинг состава
 	arrayFottballer, err := footballer.GetFootballers()
 	if err != nil {
-		log.Println("ошибка при подсчете рейтинга состава")
+		log.Println("ошибка при подсчете рейтинга состава", err.Error())
 		return CreateFootbalClubAndSquadRequest{}, err
 	}
 
-	var rating int 
+	var rating int
 	var sum int
 	for _, footballer := range arrayFottballer {
 		sum += footballer.Rating
 	}
 
-	rating = sum/len(arrayFottballer)
+	rating = sum / len(arrayFottballer)
 
+	for _, footballer_id := range request.FootballerIDs {
+		squad_id := generateIdForSquad()
 
-	// 2. Создание состава
-	var squadID int
-	err = tx.QueryRow(`
-		INSERT INTO squad (name_squad, rating_squad, id_football_club)
-		VALUES ($1, $2, $3)`, request.SquadName, rating, clubID).Scan(&squadID)
-	if err != nil {
-		log.Println("ошика при создании состава")
-		return CreateFootbalClubAndSquadRequest{}, err
+		
+		_, err = tx.Exec(`
+			INSERT INTO squad (squad_id, name_squad, rating_squad, id_footballer, id_football_club)
+			VALUES ($1, $2, $3, $4, $5)
+			RETURNING squad_id`, squad_id, request.SquadName, rating, footballer_id, clubID)
+		if err != nil {
+			log.Println("ошика при создании состава", err.Error())
+			return CreateFootbalClubAndSquadRequest{}, err
+			} 
 	}
-
-	// 3. Привязка футболистов к составу
-	// Формируем строку для IN запросов
-	footballerIDs := make([]interface{}, len(request.FootballerIDs))
-	for i, id := range request.FootballerIDs {
-		footballerIDs[i] = id
-	}
-	placeholders := strings.Repeat("$", len(request.FootballerIDs))
-	placeholders = strings.Join(strings.Split(placeholders, ""), ", ")
-	query := fmt.Sprintf(`
-		INSERT INTO squad_football_club (id_squad, id_football_club)
-		SELECT $1, $2
-		FROM unnest(%s) AS footballer_id;
-	`, placeholders)
-
-	_, err = tx.Exec(query, append([]interface{}{squadID, clubID}, footballerIDs...)...)
-	if err != nil {
-		log.Println("ошибка при ривязке футболистов к составу")
-		return CreateFootbalClubAndSquadRequest{}, err
-	}
-
 	// Коммит транзакции
 	err = tx.Commit()
 	if err != nil {
@@ -77,4 +59,9 @@ func CreateFootballClubAndSquad(request CreateFootbalClubAndSquadRequest) (Creat
 		return CreateFootbalClubAndSquadRequest{}, err
 	}
 	return request, nil
+}
+
+func generateIdForSquad()int {
+	timeU := time.Now().Unix()
+	return int(timeU)
 }
--- lab_4/server/src/settings/settingsdb.go
+++ lab_4/server/src/settings/settingsdb.go
@@ -33,28 +33,20 @@ CREATE TABLE coach (
 );
 
 CREATE TABLE football_club (
-	football_club_id 	SERIAL PRIMARY KEY,
-	name_clab CHARACTER VARYING(30)  NOT NULL,
+	football_club_id 	SERIAL       PRIMARY KEY,
+	name_club CHARACTER VARYING(30)  NOT NULL,
 	id_coach INT NOT NULL,
 	CONSTRAINT fk_coach FOREIGN KEY(id_coach) REFERENCES coach(coach_id)
 );
 
 CREATE TABLE squad (
-	squad_id		SERIAL PRIMARY KEY,
-	name_squad	CHARACTER VARYING(30)			NOT NULL,
+	squad_id		INT,
+	name_squad	    CHARACTER VARYING(30)	NOT NULL,
 	rating_squad	INT						NOT NULL,
 	id_footballer	INT,
 	id_football_club	INT,
 	CONSTRAINT fk_footballer FOREIGN KEY(id_footballer) REFERENCES footballer(footballer_id),
 	CONSTRAINT fk_football_club FOREIGN KEY(id_football_club) REFERENCES football_club(football_club_id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE squad_football_club (
-	id_squad INT NOT NULL,
-	id_football_club INT NOT NULL,
-	CONSTRAINT fk_squad FOREIGN KEY(id_squad) REFERENCES squad(squad_id),
-	CONSTRAINT fk_football_club FOREIGN KEY(id_football_club) REFERENCES football_club(football_club_id)
 );
 
 INSERT INTO coach (first_name, last_name) VALUES
